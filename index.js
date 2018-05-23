cf9e2bdd421686d0efb8c5a8e123e8299cb339da
Shayne Brady <stbrady@hotmail.com.au>
2018-05-24 00:58:02 +1000

    added cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var cors = require('cors');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
